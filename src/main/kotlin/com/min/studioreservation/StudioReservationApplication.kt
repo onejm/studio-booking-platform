9f9edbc612d3dfacf2350e846cf230add1fb032c
seaotter <jeongmin0324@gmail.com>
2026-02-25 19:46:38 +0900

    🎉 Spring Boot 초기 프로젝트 구성

---
+++ src/main/kotlin/com/min/studioreservation/StudioReservationApplication.kt
@@ -0,0 +1,11 @@
+package com.min.studioreservation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudioReservationApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudioReservationApplication>(*args)
+}
---
+++ src/test/kotlin/com/min/studioreservation/StudioReservationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.min.studioreservation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudioReservationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
